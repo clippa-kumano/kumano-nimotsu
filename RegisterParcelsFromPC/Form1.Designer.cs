0e045e81abc8ba5ed821d087cf8f60bc8542fba3
clippa-ceal <clippa_ceal@yahoo.co.jp>
2021-08-22 22:11:29 +0900

    First Commit

---
+++ RegisterParcelsFromPC/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace RegisterParcelsFromPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(112, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(595, 318);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_a1;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_a2;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_a3;
+            this.pictureBox3.Location = new System.Drawing.Point(29, 144);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_a4;
+            this.pictureBox4.Location = new System.Drawing.Point(29, 204);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_b12;
+            this.pictureBox5.Location = new System.Drawing.Point(29, 264);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_b3;
+            this.pictureBox6.Location = new System.Drawing.Point(29, 324);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_b4;
+            this.pictureBox7.Location = new System.Drawing.Point(29, 384);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_c12;
+            this.pictureBox8.Location = new System.Drawing.Point(29, 444);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_c34;
+            this.pictureBox9.Location = new System.Drawing.Point(29, 504);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_rinkyapa;
+            this.pictureBox10.Location = new System.Drawing.Point(29, 564);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox10.TabIndex = 10;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::RegisterParcelsFromPC.Properties.Resources.tab_other;
+            this.pictureBox11.Location = new System.Drawing.Point(29, 624);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(100, 54);
+            this.pictureBox11.TabIndex = 11;
+            this.pictureBox11.TabStop = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(733, 24);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.RowTemplate.Height = 41;
+            this.dataGridView2.Size = new System.Drawing.Size(523, 414);
+            this.dataGridView2.TabIndex = 12;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(878, 654);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 43);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Yu Gothic UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(983, 654);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 43);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(762, 664);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "事務当番";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(167, 743);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(362, 27);
+            this.textBox3.TabIndex = 16;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::RegisterParcelsFromPC.Properties.Resources.search;
+            this.pictureBox12.Location = new System.Drawing.Point(535, 737);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox12.TabIndex = 17;
+            this.pictureBox12.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 816);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox12;
+    }
+}
+
---
+++ RegisterParcelsFromPC/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RegisterParcelsFromPC
+{
+    public partial class Form1 : Form
+    {
+        string connStr = @"Server=.\SQLEXPRESS;Initial Catalog=parcels;UID=sa;PWD=kumano";
+        int ryoseiTable_block = 1;
+  
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ShowCellInformation(sender, e, "CellContentClick");
+        }
+
+        void ShowCellInformation(object sender, DataGridViewCellEventArgs args, string boxTitle)
+        {
+
+            DataGridView g = sender as DataGridView;
+
+            if (g != null)
+            {
+                int col = args.ColumnIndex;
+                int row = args.RowIndex;
+
+                //
+                // クリックがヘッダー部分などの場合はインデックスが-1となります。
+                //現状はcol 0:部屋番号、1:氏名、2:荷物数、3:登録、4:受取
+                //
+                DialogResult result;
+                if (row >= 0 && col >=0&col<=2)
+                {
+                    result =MessageBox.Show(string.Format("行：{0}, 列：{1}, 値：{2}", row, col, g[col, row].Value), boxTitle, MessageBoxButtons.OK);
+                }
+                if (row >= 0 && col ==3)//登録
+                {
+                    result = MessageBox.Show(g[1, row].Value+"さんの荷物を登録します。", boxTitle, MessageBoxButtons.OKCancel);
+                    if (col == 3 && result == DialogResult.OK)
+                    {
+                        register(1, g[1, row].Value.ToString(), g[0, row].Value.ToString(),1);
+                    }
+                }
+                if (row >= 0 && col == 4)//受取
+                {
+                    result = MessageBox.Show(string.Format("行：{0}, 列：{1}, 値：{2}", row, col, g[col, row].Value), boxTitle, MessageBoxButtons.OKCancel);
+                }
+
+
+                
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            show_parcels_eventTable();
+        }
+
+        void show_ryoseiTable()
+        {
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(connStr))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT [room_name] as '部屋番号',[ryosei_name] as '氏名',[parcels_current_count] as '荷物数', '登録' as '登録','受取' as '受取' FROM [parcels].[dbo].[ryosei] where block_id='" + ryoseiTable_block+"'";
+                var sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            dataGridView1.DataSource = dt;
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+        void show_parcels_eventTable()
+        {
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(connStr))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT top(50) case [event_type] when 1 then '登録' when 2 then '受取' else 'その他' end  as '操作種類',  [room_name] as '部屋番号',[ryosei_name] as '氏名　　　',[created_at] as '操作時刻',[note] as '特記事項', parcel_uid from parcel_event order by created_at desc";
+                var sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            dataGridView2.DataSource = dt;
+            this.dataGridView2.Columns["操作時刻"].DefaultCellStyle.Format = "MM/dd HH:mm:ss";
+            this.dataGridView2.Columns["parcel_uid"].Visible = false;
+
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ryosei_id"></param>
+        /// <param name="ryosei_name"></param>
+        /// <param name="event_type">1:登録、2:受取</param>
+        void register(int ryosei_id, string ryosei_name, string room_name, int event_type)
+        {
+
+            //参考：ttps://www.ipentec.com/document/csharp-sql-server-connect-exec-sql
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                //parcelテーブル用
+                string v_owner_room_name, v_owner_ryosei_name, v_register_datatime, v_register_staff_id, v_register_staff_room_name,v_register_staff_ryosei_name, v_is_fridge;
+                
+                v_owner_room_name = room_name;
+                v_owner_ryosei_name = ryosei_name;
+                DateTime dt = DateTime.Now;
+                v_register_datatime = dt.ToString();
+                v_register_staff_id = "1";
+                v_register_staff_room_name =textBox1.Text;
+                v_register_staff_ryosei_name =textBox2.Text;
+                v_is_fridge = "0";
+                string query_insert_parcels = "insert into [parcels] (owner_room_name,owner_ryosei_name,register_datatime, register_staff_id, register_staff_room_name,register_staff_ryosei_name,is_fridge) values ('"+v_owner_room_name+"','" +v_owner_ryosei_name + "','" + v_register_datatime + "'," + v_register_staff_id + ",'" +v_register_staff_room_name+"','"+ v_register_staff_ryosei_name + "'," + v_is_fridge+")";
+                SqlCommand command_parcels = new SqlCommand(query_insert_parcels, conn);
+                command_parcels.ExecuteNonQuery();
+
+                //eventテーブル用
+                string query_get_parcel_uid = "select top(1) * from parcels order by uid desc";
+                SqlCommand command_parcels_event_1 = new SqlCommand(query_get_parcel_uid, conn);
+                SqlDataReader sdr1 = command_parcels_event_1.ExecuteReader();
+                sdr1.Read();
+                DateTime _confirm_register_datatime = (DateTime)sdr1["register_datatime"];
+                int _parcel_uid = (int)sdr1["uid"];
+                if (_confirm_register_datatime.ToString() == v_register_datatime)
+                {
+                    //先頭の行が実行後のやつかどうか不安なのでチェックしたかった
+                }
+                sdr1.Close();
+                command_parcels_event_1.Dispose();
+
+                string v_created_at, v_event_type, v_parcel_uid, v_room_name, v_ryosei_name;
+                v_created_at = v_register_datatime;
+                v_event_type = event_type.ToString();
+                v_parcel_uid = _parcel_uid.ToString();
+                v_room_name = room_name;
+                v_ryosei_name = ryosei_name;
+
+                string query_insert_event = "insert into [parcel_event] (created_at,event_type,parcel_uid,room_name,ryosei_name) values ('"+v_created_at+"',"+v_event_type + "," + v_parcel_uid + ",'" + v_room_name + "','" + v_ryosei_name+"')";
+                SqlCommand command_parcels_event = new SqlCommand(query_insert_event, conn);
+                command_parcels_event.ExecuteNonQuery();
+
+                //ryoseiテーブル用
+                string query_get_ryosei ="select * from ryosei where ryosei_name='"+v_owner_ryosei_name+"'";
+                SqlCommand command_ryosei_1 = new SqlCommand(query_get_ryosei, conn);
+                SqlDataReader sdr2 = command_ryosei_1.ExecuteReader();
+                sdr2.Read();
+                int _parcels_current_count = (int)sdr2["parcels_current_count"];
+                int _parcels_total_count = (int)sdr2["parcels_total_count"];
+                sdr2.Close();
+                command_ryosei_1.Dispose();
+
+                string v_parcels_current_count, v_parcels_total_count, v_last_event_datatime;
+                v_parcels_current_count = (_parcels_current_count + 1).ToString();
+                v_parcels_total_count = (_parcels_total_count + 1).ToString();
+                v_last_event_datatime = v_register_datatime;
+                string query_update_ryosei = "update ryosei set parcels_current_count=" + v_parcels_current_count + ",parcels_total_count=" + v_parcels_total_count + ",last_event_datatime='" + v_last_event_datatime + "' where ryosei_name='"+ryosei_name+"'";
+                SqlCommand command_ryosei_2 = new SqlCommand(query_update_ryosei, conn);
+                command_ryosei_2.ExecuteNonQuery();
+                conn.Close();
+            }
+            show_parcels_eventTable();
+            show_ryoseiTable();
+
+
+        }
+        void change_blockTabImage(int n)
+        {           
+            
+            //リソースマネージャーを使う
+            System.Reflection.Assembly assembly;
+
+            assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            //Project1はプロジェクト名
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager
+            ("RegisterParcelsFromPC.Properties.Resources", assembly);
+
+            //指定されたリソースにインポートした画像を読み込む
+            //JANは画像名　Januaryの一月やね。
+
+            Bitmap A1 = (Bitmap)rm.GetObject("tab_a1");
+            Bitmap A2 = (Bitmap)rm.GetObject("tab_a2");
+            Bitmap A1_B = (Bitmap)rm.GetObject("tab_a1_B");
+            Bitmap A2_B = (Bitmap)rm.GetObject("tab_a2_B");
+
+
+            pictureBox1.Image = A1;
+            pictureBox2.Image = A2;
+            /*
+            pictureBox3.Image = A3;
+            pictureBox4.Image = A4;
+            pictureBox5.Image = B12;
+            */
+            if (n == 1) pictureBox1.Image = A1_B;
+            if (n == 2) pictureBox2.Image = A2_B;
+
+
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ryoseiTable_block = 1;
+            show_ryoseiTable();
+            change_blockTabImage(1);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            ryoseiTable_block = 2;
+            show_ryoseiTable();
+            change_blockTabImage(2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            ryoseiTable_block = 3;
+            show_ryoseiTable();
+            change_blockTabImage(0);
+        }
+         
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            ryoseiTable_block = 4;
+            show_ryoseiTable();
+            change_blockTabImage(0);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            ryoseiTable_block = 5;
+            show_ryoseiTable();
+            change_blockTabImage(0);
+        }
+
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
